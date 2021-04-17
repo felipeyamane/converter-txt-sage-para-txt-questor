b62f8064c27a5bfdae73eb3e9934e6add34823ab
felipeyamane <lipeyamane@gmail.com>
2021-04-17 19:30:26 -0300

    logica finalizada

--- converte.py
+++ converte.py
@@ -1,17 +1,22 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
+from tkinter.filedialog import asksaveasfile
+from tkinter import filedialog
 
 Tk().withdraw()  # Isto torna oculto a janela principal
 filename = askopenfilename()  # Isto te permite selecionar um arquivo
 print(filename)  # printa o arquivo selecionado
-
+arq = list()
 '''/*---------------------*/'''
 
-d = {}
 with open(filename) as f:
     for line in f:
-        (key, val, val2, val3, val4, val5, val6) = line.split("|")
-        d[int(key)] = val, val2, val3, val4, val5, val6
-        teste = (f'001;0{line[0:5]};{line[6:9]};00000;{line[18:27]};')
-        print(teste)
-#print(f'001;{d}/n')
+        file = (f'001;0{line[0:5]};{line[6:9]};00000;{line[18:27]};\n')
+        arq.append(file)
+        print(arq)
+path = filedialog.asksaveasfilename(filetypes = [('Texto', '.txt')])
+print(path)
+arquivo = open(path, "a")
+print(arquivo)
+arquivo.writelines(arq)
