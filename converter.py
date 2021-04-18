93e5e8922e178cd725c756a1e8ac2e5e3919744a
felipeyamane <lipeyamane@gmail.com>
2021-04-17 21:04:05 -0300

    inclusão de comentários

--- converter.py
+++ converter.py
@@ -9,16 +9,14 @@ filename = askopenfilename()  # Isto te permite selecionar um arquivo
 print('----------------------------------------------------------------------------------')
 print(f'Abrindo arquivo em: {filename}')  # printa o arquivo selecionado
 arq = list()
-linha = 0
-execu = 0
+
 '''/*---------------------*/'''
 
 with open(filename) as f:
     for line in f:
-        file = (f'001;0{line[0:5]};{line[6:9]};00000;{line[18:27]};\n')
-        arq.append(file)
-path = filedialog.asksaveasfilename(filetypes=[('Texto', '.txt')])
-path += '.txt'
+        file = (f'001;0{line[0:5]};{line[6:9]};00000;{line[18:27]};\n') #reconcatena arquivo
+        arq.append(file) #grava linha a linha com os dados reconcatenados
+path = filedialog.asksaveasfilename(filetypes=[('Texto', '.txt')]) + '.txt' #determina local de salvamente do novo arquivo
 arquivo = open(path, "a")
 print('----------------------------------------------------------------------------------')
 print(f'Arquivo convertido com sucesso, salvo em: {path}')
