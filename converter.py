ddb6cf615791f666a63b8a1a7b2e1afdfedbf8f9
felipeyamane <lipeyamane@gmail.com>
2021-04-17 21:00:02 -0300

    finalizado programa incluso inclone

--- converter.py
+++ converter.py
@@ -6,22 +6,22 @@ from time import sleep
 
 Tk().withdraw()  # Isto torna oculto a janela principal
 filename = askopenfilename()  # Isto te permite selecionar um arquivo
-print(filename)  # printa o arquivo selecionado
+print('----------------------------------------------------------------------------------')
+print(f'Abrindo arquivo em: {filename}')  # printa o arquivo selecionado
 arq = list()
 linha = 0
 execu = 0
 '''/*---------------------*/'''
 
-with open(filename) as f:  
-
+with open(filename) as f:
     for line in f:
         file = (f'001;0{line[0:5]};{line[6:9]};00000;{line[18:27]};\n')
         arq.append(file)
-path = filedialog.asksaveasfilename(filetypes = [('Texto', '.txt')])
+path = filedialog.asksaveasfilename(filetypes=[('Texto', '.txt')])
 path += '.txt'
 arquivo = open(path, "a")
-print('-------------------------------------------------------------------')
+print('----------------------------------------------------------------------------------')
 print(f'Arquivo convertido com sucesso, salvo em: {path}')
-print('-------------------------------------------------------------------')
+print('----------------------------------------------------------------------------------')
 sleep(5)
 arquivo.writelines(arq)
